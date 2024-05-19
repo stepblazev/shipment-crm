086aac647b61e85b4ebeb9d41fdb66d36efe2b66
Eugene Ohvatuk <ohvatuki@gmail.com>
2024-05-19 12:44:26 +0300

    ...

--- src/app/components/pages/fleet/fleet-assets/fleet-assets.component.ts
+++ src/app/components/pages/fleet/fleet-assets/fleet-assets.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AssetService } from 'src/app/models/assets/asset.service';
 
 @Component({
   selector: 'app-fleet-assets',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './fleet-assets.component.html',
-  styleUrl: './fleet-assets.component.scss'
+  styleUrl: './fleet-assets.component.scss',
 })
 export class FleetAssetsComponent {
+  constructor(public readonly assetService: AssetService) {}
 
+  ngOnInit(): void {
+    this.assetService.getList();
+  }
 }
